dd95fb8176f9419f6f1e63a80acfa8524050fb01
T.B.S <ewprjkt@proton.me>
2022-08-05 22:09:10 +0700

    TC: regenerate patch

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
-cd $(pwd)/llvmTC
+git clone https://github.com/Tiktodz/elwizTC -b llvm-tc $(pwd)/elwizTC
+cd $(pwd)/elwizTC
 bash build-tc.sh
